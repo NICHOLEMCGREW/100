6fb0c91d151492b8d4df0a692066cd752c1a01ac
Nichole McGrew <nicholemcgrew@gmail.com>
2024-03-18 08:01:39 -0500

    connected frontend

---
+++ client/src/index.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import './style.css';
+import App from './App';
+import Root from './routes/Root';
+import ErrorPage from './routes/ErrorPage';
+import Login from './routes/Login';
+import Signup from './routes/Signup';
+import Index from './routes/Index';
+import { Profile } from './routes/Profile';
+import Logout from './routes/Logout';
+import Feed from './routes/Feed';
+import Post from './routes/Post';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Index />
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/logout",
+        element: <Logout />,
+      },
+      {
+        path: "/signup",
+        element: <Signup />,
+      },
+      {
+        path: "/profile",
+        element: <Profile />,
+      },
+      {
+        path: "/feed",
+        element: <Feed />,
+      },
+      {
+        path: "/post/:id",
+        element: <Post />,
+      },
+    ]
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
--- controllers/auth.js
+++ controllers/auth.js
@@ -43,7 +43,7 @@ exports.logout = (req, res) => {
   req.logout(() => {
     console.log('User has logged out.')
   })
-  req.session.destroy((err) => {
+  req..destroy((err) => {
     if (err)
       console.log("Error : Failed to destroy the session during logout.", err);
     req.user = null;
